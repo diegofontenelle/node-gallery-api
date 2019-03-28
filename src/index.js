e4cfb776fac0518fa1c9e4634dbf48d525a11ca6
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2019-03-28 01:21:16 -0300

    Changing storage to aws S3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
 const routes = require("./routes");
 const morgan = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
+
+/**
+ * Database setup
+ */
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 app.use(routes);
 
 app.listen(5000);
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,31 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-routes.get("/", (req, res) => {
-  return res.json({ hello: "xd!" });
+const Post = require("./models/Post");
+
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
 });
 
-routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
-  console.log(req.file);
-  return res.json({ hello: "xd!" });
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, key, location: url } = req.file;
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  });
+  return res.json(post);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
 });
 
 module.exports = routes;
